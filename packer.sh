f14d84d86ea36a05136aedaed01ac021cf61695e
N4T4NM <natanaelmarian@hotmail.com>
2025-07-16 00:22:15 -0300

    Update zip name

--- packer.sh
+++ packer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -j RepoSoundboard.zip RepoSoundboard/bin/Release/netstandard2.1/Audio.dll RepoSoundboard/bin/Release/netstandard2.1/RepoSoundboard.dll icon.png manifest.json README.md
+zip -j NatanM-RepoSoundboard.zip RepoSoundboard/bin/Release/netstandard2.1/Audio.dll RepoSoundboard/bin/Release/netstandard2.1/RepoSoundboard.dll icon.png manifest.json README.md
