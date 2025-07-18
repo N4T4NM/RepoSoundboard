af3062504208f895f9297509e19f0bf4d754990a
N4T4NM <natanaelmarian@hotmail.com>
2025-07-17 21:04:41 -0300

    Pack changelog

--- packer.sh
+++ packer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -j NatanM-RepoSoundboard.zip RepoSoundboard/bin/Release/netstandard2.1/Audio.dll RepoSoundboard/bin/Release/netstandard2.1/RepoSoundboard.dll icon.png manifest.json README.md
+zip -j NatanM-RepoSoundboard.zip RepoSoundboard/bin/Release/netstandard2.1/Audio.dll RepoSoundboard/bin/Release/netstandard2.1/RepoSoundboard.dll icon.png manifest.json README.md CHANGELOG.md
